d3d69f3d2ec3f845ce771ba35b0fdccc243f96a5
minhluan2801 <166634948+minhluan2801@users.noreply.github.com>
2024-04-20 14:00:02 +0700

    update

---
+++ data/duongsat_5.js
@@ -0,0 +1,1 @@
+var json_duongsat_5 = {"type":"FeatureCollection","name":"duongsat_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/vnhlong1_2.js
@@ -0,0 +1,1 @@
+var json_vnhlong1_2 = {"type":"FeatureCollection","name":"vnhlong1_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
